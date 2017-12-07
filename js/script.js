8170af9f0311b49103049aa05b4f45bec6230b0d
tabby42 <irene.theiss@gmx.at>
2017-12-07 13:34:26 +0100

    comment out prompts for the time being

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 (function(){ 
-	var playerOne = prompt("Player 1, please enter your name:");
-	var playerTwo = prompt("Player 2, please enter your name:");
-	//console.log(document.getElementById("player-1"));
-	document.getElementById("player-1").textContent = playerOne;
-	document.getElementById("player-2").textContent = playerTwo;
+	// var playerOne = prompt("Player 1, please enter your name:");
+	// var playerTwo = prompt("Player 2, please enter your name:");
+	// //console.log(document.getElementById("player-1"));
+	// document.getElementById("player-1").textContent = playerOne;
+	// document.getElementById("player-2").textContent = playerTwo;
 
 
 
